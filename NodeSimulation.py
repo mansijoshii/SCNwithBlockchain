7cbc12332ab2450fd637833fc44bd32406df8c3e
Mansi Joshi <mansij1999@gmail.com>
2020-04-14 05:44:49 +0530

    ThroughputGraph

--- NodeSimulation.py
+++ NodeSimulation.py
@@ -74,29 +74,35 @@ manufacturer1.register_node(retailer2)
 # Measuring Performance
 transaction_cnt = []
 transaction_time = []
+transaction_tps = []
 for i in range(NUM_SIMULATIONS):
     print("Running Simulation " + str(i+1))
     cnt = 0
     t1 = time.time()
+    t2 = 0
     while 1:
         t2 = time.time()
-        if (t2 - t1 >= 3):
+        if (t2 - t1 >= 0.01 * (i+1)):
             break
         if (cnt % 2 == 0):
             manufacturer1.transaction("send", "wholesaler1", 4)
         elif (cnt % 2 == 1):
             wholesaler1.transaction("send", "retailer1", 4)
         cnt += 1
+    t3 = t2-t1
     transaction_cnt.append(cnt)
-    transaction_time.append(time.time()-t1)
+    transaction_time.append(t3)
+    transaction_tps.append(cnt/t3)
+    time.sleep(2)
 
 tps = sum(transaction_cnt)/sum(transaction_time)
 
 print("Average throughput over " + str(NUM_SIMULATIONS) + " simulations is " + 
     str(tps) + " transactions per second")
 
-plt.plot(transaction_cnt, transaction_time)
+plt.plot(transaction_cnt, transaction_tps)
+
 plt.xlabel('Number of Transactions') 
-plt.ylabel('Confirmation time')  
-plt.title('Analysis of transactions and confirmation times') 
+plt.ylabel('TPS')  
+plt.title('Analysis of throughput') 
 plt.show() 
